914f3f9b1cf0b8069a5207f11fd93ecdb64eaf02
Daniel Mox <dcm64@drexel.edu>
2017-05-22 14:01:03 -0400

    Compute skeletons for grid_mapping::OccupancyGrid messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include "csqmi_planning/common.h"
 #include "csqmi_planning/thinning.hpp"
 
-using namespace std;
-using namespace cv;
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <grid_mapping/OccupancyGrid.h>
 
 int main(int argc, char** argv)
 {
-  Mat img = loadMat("data/map.bin");
+  // read OccupancyGrid from saved bag
+  cv::Mat img;
+  rosbag::Bag bag;
+  bag.open("data/pan_grid.bag", rosbag::bagmode::Read);
+  for (auto m : rosbag::View(bag)) {
+    grid_mapping::OccupancyGridConstPtr grid;
+    grid = m.instantiate<grid_mapping::OccupancyGrid>();
+    if (grid) {
+      occupancyGridToMat(grid, img);
+      break;
+    }
+  }
 
   // show image
-  Mat skeleton;
+  cv::Mat skeleton;
   computeSkeleton(img, skeleton);
   img.setTo(50, skeleton);
   displayImageComplement(img, "Map");
-  //displayImage(skeleton, "Skeleton");
+  displayImage(skeleton, "Skeleton");
 
   return 0;
 }
--- src/thinning.cpp
+++ src/thinning.cpp
@@ -1,36 +1,35 @@
+#include "csqmi_planning/thinning.hpp"
+#include "csqmi_planning/common.h"
+
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <stack>
-#include "csqmi_planning/thinning.hpp"
-#include "csqmi_planning/common.h"
 
-using namespace std;
-using namespace cv;
-
-ostream& operator<<(ostream& out, const Vec4i& v) {
+std::ostream& operator<<(std::ostream& out, const cv::Vec4i& v) {
   out << "(" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << ")";
   return out;
 }
 
-void computeSkeleton(Mat& img_in, Mat& img_out)
+void computeSkeleton(cv::Mat& img_in, cv::Mat& img_out)
 {
   img_in.convertTo(img_in, CV_8UC1);
 
-  // pad image and invert the colors
-  Mat img;
-  copyMakeBorder(1-img_in, img, 1, 1, 1, 1, BORDER_CONSTANT, Scalar(0));
-  threshold(img, img, 0, 1, THRESH_BINARY);
+  // pad image, invert colors, and convert to binary image
+  cv::Mat img;
+  cv::copyMakeBorder(img_in, img, 1, 1, 1, 1, cv::BORDER_CONSTANT, 0);
+  cv::threshold(img, img, 10, 1, cv::THRESH_BINARY_INV); // assumes [0, 100]
 
   // erode image to cut down on noise
-  erode(img, img, getStructuringElement(MORPH_RECT, Size(3,3)), Point(-1,-1), 4);
+  cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3,3));
+  cv::erode(img, img, se, cv::Point(-1,-1), 4);
 
   auto cond1 = [] (int p2, int p4, int p6, int p8) { 
     return (p2*p4*p6 != 0) || (p4*p6*p8 != 0); };
   auto cond2 = [] (int p2, int p4, int p6, int p8) { 
     return (p2*p4*p8 != 0) || (p2*p6*p8 != 0); };
 
-  Mat prev_img = img.clone();
+  cv::Mat prev_img = img.clone();
   while (true) {
     if (subIteration(img, prev_img, cond1) > 0)
       img.copyTo(prev_img);
@@ -69,30 +68,30 @@ void computeSkeleton(Mat& img_in, Mat& img_out)
   }
 
   // find connected skeleton components
-  Mat tmp = img.clone();
-  vector<vector<Point>> cc;
+  cv::Mat tmp = img.clone();
+  std::vector<std::vector<cv::Point>> cc;
   int largest_comp = 0; // size of largest vector in cc
   for (int i = 1; i < tmp.rows-1; ++i) {
     for (int j = 1; j < tmp.cols-1; ++j) {
       if (tmp.at<unsigned char>(i,j) == 1) {
 
-        vector<Point> comp; // cells in current component
-        stack<Point> next_point; // next cells to search
-        next_point.push(Point(j,i));
+        std::vector<cv::Point> comp; // cells in current component
+        std::stack<cv::Point> next_point; // next cells to search
+        next_point.push(cv::Point(j,i));
 
         // find all connected pixels
         while (!next_point.empty()) {
-          Point p = next_point.top();
+          cv::Point p = next_point.top();
           comp.push_back(p);
           next_point.pop();
           tmp.at<unsigned char>(p) = 0;
   
-          Mat roi = tmp(Rect(p, Size(3,3))-Point(1,1));
-          vector<Point> neighbors;
-          findNonZero(roi, neighbors);
+          cv::Mat roi = tmp(cv::Rect(p, cv::Size(3,3))-cv::Point(1,1));
+          std::vector<cv::Point> neighbors;
+          cv::findNonZero(roi, neighbors);
 
           for (auto n : neighbors) {
-            next_point.push(n+p-Point(1,1));
+            next_point.push(n+p-cv::Point(1,1));
           }
         }
 
@@ -110,28 +109,5 @@ void computeSkeleton(Mat& img_in, Mat& img_out)
         img.at<unsigned char>(point) = 0;
 
   // store results in output array with pad and threshold removed
-  img_out = Mat(img, Rect(1, 1, img.cols-2, img.rows-2)) * 255;
+  img_out = cv::Mat(img, cv::Rect(1, 1, img.cols-2, img.rows-2)) * 255;
 }
-
-/*
-void pruneSkeleton(Mat skel, const Mat nav_map, const double res, const double radius)
-{
-  Mat out = Mat::zeros(skel.size(), CV_8UC1);
-  int min_dist = ceil(radius/res);        
-
-  for (int i = min_dist; i < skel.rows-min_dist-1; i++) {
-    for (int j = min_dist; j < skel.cols-min_dist-1; j++) {
-      if (skel.at<unsigned char>(i,j) > 0) {
-        Rect r(j-min_dist, i-min_dist, 2*min_dist+1, 2*min_dist+1);
-        double min_val = 0;
-        minMaxLoc(nav_map(r), &min_val);
-
-        if (min_val > 0.0)
-          out.at<unsigned char>(i, j) = 1;
-      }
-    }
-  }
-
-  skel &= out;
-}
-*/
