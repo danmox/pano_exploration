1fe5711609e4cba24dfe2fc328416184b25716b2
Daniel Mox <dcm64@drexel.edu>
2017-05-22 14:00:15 -0400

    Add function to read grid_mapping::OccupancyGrid msgs from rosbags

--- include/csqmi_planning/common.h
+++ include/csqmi_planning/common.h
@@ -2,10 +2,12 @@
 #define COMMON_H
 
 #include <opencv2/opencv.hpp>
+#include <grid_mapping/OccupancyGrid.h>
 #include <string.h>
 
 void displayImageComplement(const cv::Mat& img, const std::string name);
 void displayImage(const cv::Mat& img, const std::string name);
 cv::Mat loadMat(std::string file);
+void occupancyGridToMat(const grid_mapping::OccupancyGridConstPtr&, cv::Mat&);
 
 #endif
--- src/common.cpp
+++ src/common.cpp
@@ -5,44 +5,75 @@
 #include <vector>
 #include <stdlib.h>
 
-using namespace std;
-using namespace cv;
-
-void displayImageComplement(const Mat& img, const string name)
+void displayImageComplement(const cv::Mat& img, const std::string name)
 {
-  namedWindow(name, WINDOW_NORMAL);
+  cv::namedWindow(name, cv::WINDOW_NORMAL);
   double min, max;
-  minMaxLoc(img, &min, &max);
-  imshow(name, 255 - img*(255/max));
-  waitKey(0);
+  cv::minMaxLoc(img, &min, &max);
+  cv::imshow(name, 255 - img*(255/max));
+  cv::waitKey(0);
 }
 
-void displayImage(const Mat& img, const string name)
+void displayImage(const cv::Mat& img, const std::string name)
 {
-  namedWindow(name, WINDOW_NORMAL);
+  cv::namedWindow(name, cv::WINDOW_NORMAL);
   double min, max;
-  minMaxLoc(img, &min, &max);
-  imshow(name, img*(255/max));
-  waitKey(0);
+  cv::minMaxLoc(img, &min, &max);
+  cv::imshow(name, img*(255/max));
+  cv::waitKey(0);
 }
 
-Mat loadMat(string file)
+cv::Mat loadMat(std::string file)
 {
-  ifstream f(file, ios::in | ios::binary | ios::ate);
+  std::ifstream f(file, std::ios::in | std::ios::binary | std::ios::ate);
   if (!f.is_open()) {
-    cout << "Error opening map.bin" << endl;
+    std::cout << "Error opening map.bin" << std::endl;
     exit(-1);
   }
 
   size_t sz = f.tellg();
-  vector<char> map_data(sz);
-  f.seekg(0, ios::beg);
+  std::vector<char> map_data(sz);
+  f.seekg(0, std::ios::beg);
   f.read(reinterpret_cast<char*>(map_data.data()), sz);
 
   if (map_data.size() != 258*292) {
-    cout << "Input data and Mat dimensions do not agree" << endl;
-    exit(-1);
+    std::cout << "Input data and Mat dimensions do not agree" << std::endl;
+    exit(EXIT_FAILURE);
   }
 
-  return Mat(292, 258, CV_8UC1, map_data.data()).t();
+  return cv::Mat(292, 258, CV_8UC1, map_data.data()).t();
+}
+
+typedef grid_mapping::OccupancyGridConstPtr GridConstPtr;
+void occupancyGridToMat(const GridConstPtr& grid, cv::Mat& img)
+{
+  // convert logodds data to occupancy data [-inf,+inf] -> [0, 100]
+  std::vector<int> map_data;
+  std::transform(grid->data.begin(), grid->data.end(), 
+      std::back_inserter(map_data), 
+      [](double a){ return static_cast<int>(100*(1.0/(1.0+exp(-a)))) ;});
+
+  // convert perspective (multi-layered) grid to 2D grid
+  size_t layer_size = grid->width*grid->height;
+  std::vector<int> map_data_2d(layer_size, 50);
+  for (size_t i = 0; i < layer_size; ++i) {
+
+    int min = 50, max = 50;
+    for (int l = 0; l < grid->layers; ++l) {
+      int val = map_data[i + l*layer_size];
+      min = val < min ? val : min;
+      max = val > max ? val : max;
+    }
+
+    // give preference to occupied cells
+    if (max > 50)
+      map_data_2d[i] = max;
+    else if (min < 50)
+      map_data_2d[i] = min;
+  }
+
+  // create cv::Mat from transformed grid data
+  img = cv::Mat(map_data_2d).reshape(0, grid->height);
+  img.convertTo(img, CV_8UC1);
+  cv::flip(img, img, 0);
 }
