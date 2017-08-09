fc93c09e54141a0278f1ca90057512e3c34b338c
Daniel Mox <dcm64@drexel.edu>
2017-08-09 18:04:26 -0400

    Separate planning and visualization components

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <grid_mapping/angle_grid.h>
+#include <grid_mapping/pixel_density_grid.h>
+#include <geometry_msgs/TransformStamped.h>
 
 #include <algorithm>
 
@@ -19,38 +21,65 @@ int main(int argc, char** argv)
     exit(EXIT_FAILURE);
   }
 
-  // read OccupancyGrid from saved bag
-  Mat grid_img;
+  // read panorama capture location from saved bag
   rosbag::Bag bag;
   bag.open(argv[1], rosbag::bagmode::Read);
-  grid_mapping::OccupancyGridConstPtr grid;
-  for (auto m : rosbag::View(bag)) {
-    grid = m.instantiate<grid_mapping::OccupancyGrid>();
-    if (grid)
+  geometry_msgs::TransformStamped pan_pose;
+  for (auto m : rosbag::View(bag, rosbag::TopicQuery("panorama_pose"))) {
+    auto msg = m.instantiate<geometry_msgs::TransformStamped>();
+    if (msg) {
+      pan_pose = *msg;
       break;
+    }
   }
+  grid_mapping::Point panorama_position(pan_pose.transform.translation.x,
+      pan_pose.transform.translation.y);
+  grid_mapping::AngleGrid ang_grid(grid_mapping::Point(0.0,0.0), 0.1, 1, 1);
+  ang_grid.insertPanorama(argv[1]);
+  grid_mapping::PixelDensityGrid px_grid(grid_mapping::Point(0.0,0.0),0.1,1,1);
+  px_grid.insertPanorama(argv[1]);
 
-  // create AngleGrid and opencv image from saved grid_mapping::OccupancyGrid
-  grid_mapping::AngleGrid ang_grid(grid);
-  occupancyGridToMat(ang_grid, grid_img);
+  //
+  // planning components
+  //
 
-  // compute skeleton and convert to image
-  Mat skeleton, color_grid_img;
-  computeSkeleton(grid_img, skeleton);
-  cvtColor(255 - grid_img*255.0/100.0, color_grid_img, CV_GRAY2RGB);
-
-  // calculate the pixel of the panorama capture location
-  Point o;
-  ang_grid.positionToSubscripts(grid_mapping::Point(0.0, 0.0), o.y, o.x);
-  vector<Point> pans(1, o);
-  pans = locatePanoramasOnSkeleton(skeleton, pans);
+  // compute skeleton
+  Mat skeleton;
+  computeSkeleton(ang_grid, skeleton, 2);
 
   // partition skeleton
+  Point po;
+  ang_grid.positionToSubscripts(panorama_position, po.y, po.x);
+  vector<Point> pans(1, po);
   vector<vector<Point>> regions = partitionSkeleton(skeleton, pans);
-  //displayRawImage(skeleton_img, "skeleton image");
 
-  // initialize CSQMI objective function
-  CSQMI objective(DepthCamera(180, 7.0, 0.45), 0.03);
+  // find the point of each region with the hightest CSQMI
+  CSQMI objective(DepthCamera(180, 6.0, 0.45), 0.03);
+  vector<Point> max_csqmi_pts;
+  vector<double> region_max_csqmi;
+  for (auto& reg : regions) {
+    vector<double> reg_mi = objective.csqmi(ang_grid, reg);
+    int idx = max_element(reg_mi.begin(), reg_mi.end()) - reg_mi.begin();
+    max_csqmi_pts.push_back(reg[idx]);
+    region_max_csqmi.push_back(reg_mi[idx]);
+  }
+
+  // choose the goal point
+  auto goal_it = max_element(region_max_csqmi.begin(), region_max_csqmi.end());
+  Point goal_px = max_csqmi_pts[goal_it - region_max_csqmi.begin()];
+  grid_mapping::Point goal = ang_grid.subscriptsToPosition(goal_px.y, goal_px.x);
+  cout << "Goal position is: " << goal << endl;
+
+  //
+  // visualization components
+  //
+
+  // create BW image of skeleton and color image of occupancy grid
+  Mat grid_img;
+  occupancyGridToMat(ang_grid, grid_img);
+  Mat color_grid_img;
+  displayRawImage(skeleton*255, "skeleton image");
+  cvtColor(255 - grid_img*255.0/100.0, color_grid_img, CV_GRAY2RGB);
 
   // find all free space pixels
   Mat free_space_img;
@@ -82,17 +111,18 @@ int main(int argc, char** argv)
   cout << "max CSQMI at " << max_point << endl;
 
   // show the panorama capture location
+  pans = locatePanoramasOnSkeleton(skeleton, pans);
   color_csqmi_img.at<Vec3b>(pans[0]) = Vec3b(0, 0, 0); // b,g,r
 
   // calculate csqmi over skeleton and mark the maximum point of each region in
   // the CSQMI results image
-  for (auto& reg : regions) {
-    vector<double> reg_mi = objective.csqmi(ang_grid, reg);
-    int idx = max_element(reg_mi.begin(), reg_mi.end()) - reg_mi.begin();
-    cout << "max region CSQMI at " << reg[idx] << endl;
-    color_csqmi_img.at<Vec3b>(reg[idx]) = Vec3b(255, 0, 0);
-  }
+  for (auto& max_pt : max_csqmi_pts)
+    color_csqmi_img.at<Vec3b>(max_pt) = Vec3b(255, 0, 0);
+  color_csqmi_img.at<Vec3b>(goal_px) = Vec3b(255, 255, 0);
 
+  Mat pixel_density_img;
+  pixelDensityGridToMat(px_grid, pixel_density_img);
+  displayRawImage(pixel_density_img, "Pixel Density Image");
   displayRawImage(color_csqmi_img, "CSQMI Reward Surface");
 
   return 0;
