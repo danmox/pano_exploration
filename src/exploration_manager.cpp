b173e229fa1399f11230b89dbb1a26084eea5a47
Daniel Mox <dcm64@drexel.edu>
2017-08-12 20:35:43 -0400

    Configure exploration loop for multi-robot case
    Add "leader" param, which determines if a robot begins exploring by capturing a
    panorama or waiting for a map to be received
    Exploration loop begins with goal planning and ends with panorama capture
    If a robot is a leader, a panorama is captured before entering the loop

--- src/exploration_manager.cpp
+++ src/exploration_manager.cpp
@@ -49,14 +49,19 @@ struct GoalIDPair
 //
 
 int robot_id;
-volatile bool received_first_map = false;
+int pan_count = 0;
+volatile bool received_new_map = false;
 string tf_prefix;
 string pan_file;
 AngleGrid ang_grid(grid_mapping::Point(0.0, 0.0), 0.1, 1, 1);
-ros::Publisher viz_map_pub;
+ros::Publisher viz_map_pub, pan_grid_pub, pan_pose_pub;
 vector<grid_mapping::Point> pan_locations;
 vector<GoalIDPair> goals;
 tf2_ros::Buffer tfBuffer;
+double scan_range_min, scan_range_max;
+std::shared_ptr<PanAC> pan_ac;
+std::shared_ptr<MoveAC> move_ac;
+grid_mapping::Point panorama_position;
 
 //
 // panorama action functions
@@ -64,7 +69,7 @@ tf2_ros::Buffer tfBuffer;
 
 void panFeedbackCB(const panorama::PanoramaFeedbackConstPtr& feedback)
 {
-  //ROS_INFO("Captured frame %d of 72", feedback->frames_captured);
+  ROS_INFO("Captured frame %d of 72", feedback->frames_captured);
 }
 
 void panActiveCB()
@@ -178,7 +183,7 @@ bool fetchTransform(string source_frame, geometry_msgs::TransformStamped& trans)
 
 void mapCB(const grid_mapping::OccupancyGridConstPtr& msg)
 {
-  received_first_map = true;
+  received_new_map = true;
 
   geometry_msgs::TransformStamped tfs;
   if (fetchTransform(msg->header.frame_id, tfs)) {
@@ -252,6 +257,63 @@ void panPoseCB(const geometry_msgs::PoseStampedConstPtr& msg)
 }
 
 //
+// exploration functions
+//
+
+/*
+ * Collect panorama, insert it into the angle grid, and publish the angle
+ * grid, 2D rviz version, and panorama capture location
+ */
+
+bool capturePanorama()
+{
+  panorama::PanoramaGoal pan_goal;
+  pan_goal.file_name = tf_prefix + "pan" + std::to_string(++pan_count);
+  pan_ac->sendGoal(pan_goal, &panDoneCB, &panActiveCB, &panFeedbackCB);
+  pan_ac->waitForResult();
+  if (pan_file.size() == 0) {
+    ROS_WARN("Panorama action returned no file");
+    --pan_count;
+    return false;
+  }
+
+  // create grid of just the panorama and publish
+  AngleGrid pan_grid(grid_mapping::Point(0.0, 0.0), 0.1, 1, 1);
+  pan_grid.range_min = scan_range_min;
+  pan_grid.range_max = scan_range_max;
+  pan_grid.insertPanorama(pan_file);
+  pan_grid_pub.publish(pan_grid.createROSMsg());
+
+  // insert the pan grid into ang grid and publish for visualization
+  ang_grid.insertPanorama(pan_file);
+  viz_map_pub.publish(ang_grid.createROSOGMsg());
+
+  // read panoaram capture location
+  rosbag::Bag panbag;
+  panbag.open(pan_file, rosbag::bagmode::Read);
+  geometry_msgs::TransformStamped pan_pose;
+  for (auto m : rosbag::View(panbag, rosbag::TopicQuery("panorama_pose"))) {
+    auto msg = m.instantiate<geometry_msgs::TransformStamped>();
+    if (msg) {
+      geometry_msgs::PoseStamped pan_pose;
+      pan_pose.header = msg->header;
+      pan_pose.header.seq = robot_id*10 + pan_count;
+      pan_pose.pose.position.x = msg->transform.translation.x;
+      pan_pose.pose.position.y = msg->transform.translation.y;
+      pan_pose.pose.position.z = 0.0;
+      pan_pose.pose.orientation = msg->transform.rotation;
+      pan_pose_pub.publish(pan_pose);
+      break;
+    }
+  }
+  panorama_position.x = pan_pose.transform.translation.x;
+  panorama_position.y = pan_pose.transform.translation.y;
+  pan_locations.push_back(panorama_position);
+
+  return true;
+}
+
+//
 // main(...)
 //
 
@@ -265,7 +327,7 @@ int main(int argc, char** argv)
   ros::NodeHandle nh, pnh("~");
   tf2_ros::TransformListener tfListener(tfBuffer);
 
-  ros::Publisher vel_pub, pan_grid_pub, skel_pub, pan_pose_pub, goal_pose_pub;
+  ros::Publisher vel_pub, skel_pub, goal_pose_pub;
   vel_pub = nh.advertise<geometry_msgs::Twist>("velocity_commands", 10);
   viz_map_pub = nh.advertise<nav_msgs::OccupancyGrid>("map_2D", 2);
   pan_grid_pub = nh.advertise<grid_mapping::OccupancyGrid>("angle_grid", 2);
@@ -274,18 +336,16 @@ int main(int argc, char** argv)
   goal_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("goal_pose", 2);
 
   int number_of_robots;
-  double scan_range_min, scan_range_max;
+  bool leader;
   if (!pnh.getParam("tf_prefix", tf_prefix) ||
       !pnh.getParam("robot_id", robot_id) ||
+      !pnh.getParam("leader", leader) ||
       !nh.getParam("/number_of_robots", number_of_robots) ||
       !nh.getParam("/scan_range_min", scan_range_min) ||
       !nh.getParam("/scan_range_max", scan_range_max)) {
     ROS_FATAL("main(...): failed to read params from server");
     exit(EXIT_FAILURE);
   }
-
-  if (robot_id == 1)
-    received_first_map = true; 
 
   // initialize angle grid for planning
   ang_grid.range_min = scan_range_min;
@@ -307,15 +367,15 @@ int main(int argc, char** argv)
    */
 
   string pan_server_name = "/" + tf_prefix + "/panorama_action_server";
-  PanAC pan_ac(pan_server_name.c_str(), true);
+  pan_ac.reset(new PanAC(pan_server_name.c_str(), true));
   ROS_INFO("Waiting for action server to start: %s", pan_server_name.c_str());
-  pan_ac.waitForServer();
+  pan_ac->waitForServer();
   ROS_INFO("%s is ready", pan_server_name.c_str());
 
   string nav_server_name = "/" + tf_prefix + "/move_base";
-  MoveAC move_ac(nav_server_name.c_str(), true);
+  move_ac.reset(new MoveAC(nav_server_name.c_str(), true));
   ROS_INFO("Waiting for action server to start: %s", nav_server_name.c_str());
-  move_ac.waitForServer();
+  move_ac->waitForServer();
   ROS_INFO("%s is ready", nav_server_name.c_str());
 
   /*
@@ -329,81 +389,39 @@ int main(int argc, char** argv)
   }
 
   /*
+   * if this robot is a leader, capture an initial panorama before beginning the
    * exploration loop
    */
 
-  int pan_count = 0;
-  while (ros::ok()) {
-
-    /*
-     * If not robot1, wait for robot1 to complete first panorama before starting
-     * to expore
-     */
-
-    while (!received_first_map) {
+  if (leader) {
+    capturePanorama();
+  } else {
+    while (!received_new_map) {
       ros::spinOnce();
       countdown.sleep();
-      ROS_INFO("robot%d is waiting for first map from robot1", robot_id);
+      ROS_INFO("robot%d is waiting for an initial map", robot_id);
     }
+  }
 
-    /*
-     * Collect panorama, insert it into the angle grid, and publish the angle
-     * grid and 2D rviz version
-     */
+  /*
+   * exploration loop
+   */
 
-    panorama::PanoramaGoal pan_goal;
-    pan_goal.file_name = tf_prefix + "pan" + std::to_string(++pan_count);
-    pan_ac.sendGoal(pan_goal, &panDoneCB, &panActiveCB, &panFeedbackCB);
-    pan_ac.waitForResult();
-    if (pan_file.size() == 0) {
-      ROS_WARN("Panorama action returned no file. Restarting");
-      --pan_count;
-      continue;
-    }
+  while (ros::ok()) {
 
-    // create grid of just the panorama and publish
-    AngleGrid pan_grid(grid_mapping::Point(0.0, 0.0), 0.1, 1, 1);
-    pan_grid.range_min = scan_range_min;
-    pan_grid.range_max = scan_range_max;
-    pan_grid.insertPanorama(pan_file);
-    pan_grid_pub.publish(pan_grid.createROSMsg());
-
-    // insert the pan grid into ang grid and publish for visualization
-    ang_grid.insertPanorama(pan_file);
-    viz_map_pub.publish(ang_grid.createROSOGMsg());
+    // get the latest information from the team
+    ros::spinOnce();
 
     /*
      * Compute next panorama capture location
      */
 
-    // get the latest information from the team
-    ros::spinOnce();
-
-    // read panoaram capture location
-    rosbag::Bag panbag;
-    panbag.open(pan_file, rosbag::bagmode::Read);
-    geometry_msgs::TransformStamped pan_pose;
-    for (auto m : rosbag::View(panbag, rosbag::TopicQuery("panorama_pose"))) {
-      auto msg = m.instantiate<geometry_msgs::TransformStamped>();
-      if (msg) {
-        geometry_msgs::PoseStamped pan_pose;
-        pan_pose.header = msg->header;
-        pan_pose.header.seq = robot_id*10 + pan_count;
-        pan_pose.pose.position.x = msg->transform.translation.x;
-        pan_pose.pose.position.y = msg->transform.translation.y;
-        pan_pose.pose.position.z = 0.0;
-        pan_pose.pose.orientation = msg->transform.rotation;
-        pan_pose_pub.publish(pan_pose);
-        break;
-      }
-    }
-    grid_mapping::Point panorama_position(pan_pose.transform.translation.x,
-        pan_pose.transform.translation.y);
-    pan_locations.push_back(panorama_position);
-
     // compute skeleton
     cv::Mat skel;
     computeSkeleton(ang_grid, skel, 2);
+
+    // get the latest information from the team
+    ros::spinOnce();
 
     // partition skel
     vector<cv::Point> pan_pixels;
@@ -450,7 +468,12 @@ int main(int argc, char** argv)
     // if no goal was found, wait for a new map and then start planning again
     if (!goal_found) {
       ROS_WARN("failed to find a goal... will wait for new map");
-      received_first_map = false;
+      received_new_map = false;
+      while (!received_new_map) {
+        ros::spinOnce();
+        countdown.sleep();
+        ROS_INFO("robot%d is waiting for a new map", robot_id);
+      }
       continue;
     }
 
@@ -481,13 +504,17 @@ int main(int argc, char** argv)
     action_goal.target_pose.header.seq = robot_id*10 + pan_count + 1;
     goal_pose_pub.publish(action_goal.target_pose);
 
-    move_ac.sendGoal(action_goal, &moveDoneCB, &moveActiveCB, &moveFeedbackCB);
-    move_ac.waitForResult();
+    move_ac->sendGoal(action_goal, &moveDoneCB, &moveActiveCB, &moveFeedbackCB);
+    move_ac->waitForResult();
+
+    /*
+     * Collect panorama, insert it into the angle grid, and publish the angle
+     * grid and 2D rviz version
+     */
+
+    capturePanorama();
 
     ROS_INFO("Completed iteration loop %d", pan_count);
-
-    // get the latest information from the team
-    ros::spinOnce();
   }
 
   return 0;
